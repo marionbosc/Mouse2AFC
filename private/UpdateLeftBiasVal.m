96f81fb05837cd958ee6e5191aa61ac9489eb041
Hatem Oraby <hatem.oraby@gmail.com>
2019-02-06 00:55:48 +0100

    Fix UpdaeLeftBias callback causing crash
    The value was assigned as string instead of a float

--- private/UpdateLeftBiasVal.m
+++ private/UpdateLeftBiasVal.m
@@ -5,5 +5,5 @@ set(LeftBiasValHandle, 'String', num2str(SliderHandler.Value));
 TaskParameters.GUI.LeftBiasVal = num2str(SliderHandler.Value);
 % Write the value to LeftBias as well so it'd be read by the
 % updateCustomDataFields function.
-TaskParameters.GUI.LeftBias = TaskParameters.GUI.LeftBiasVal;
+TaskParameters.GUI.LeftBias = SliderHandler.Value;
 end
