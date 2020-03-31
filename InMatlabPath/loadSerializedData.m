3a9197efda388470814dd1e84cca2fb39c17b4e3
Hatem Oraby <hatem.oraby@gmail.com>
2020-03-31 15:07:09 +0200

    loadSerializeData: Suppress debug output
    The helping output create much noise. Suppress it for now.

--- InMatlabPath/loadSerializedData.m
+++ InMatlabPath/loadSerializedData.m
@@ -1,7 +1,7 @@
 function [next_data_start, data] = loadSerializedData(mapped_file, data_start)
     data_end = data_start + 4 - 1;
     data_length = typecast(mapped_file.Data(data_start:data_end),'uint32');
-    disp("Custom data length: " + string(data_length));
+    % disp("Custom data length: " + string(data_length));
     data_start = data_end + 1; data_end = data_start + data_length - 1;
     serialized_data = mapped_file.Data(data_start:data_end);
     data = hlp_deserialize(serialized_data);
