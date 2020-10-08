9474b00448f20df7d2fe68162a7eb3e83200b2bc
Hatem Oraby <hatem.oraby@gmail.com>
2020-10-08 14:56:33 +0200

    mat_reader: Synchoronize more GUI fields from Matlab data files

--- report/mat_reader.py
+++ report/mat_reader.py
@@ -18,9 +18,10 @@ def loadFiles(files_patterns=["*.mat"], stop_at=10000, mini_df=False):
     IMP_GUI_COLS = ["GUI_ExperimentType", "GUI_StimAfterPokeOut",
         "GUI_CatchError", "GUI_PercentCatch", "GUI_FeedbackDelayMax",
         "GUI_MinSampleType", "GUI_MinSample", "GUI_MinSampleMin",
-        "GUI_MinSampleMax", "GUI_RewardAfterMinSampling",
+        "GUI_MinSampleMax", "GUI_RewardAfterMinSampling", "GUI_StimulusTime",
         "GUI_FeedbackDelaySelection", "GUI_CalcLeftBias", "GUI_MouseState",
-        "GUI_MouseWeight"]
+        "GUI_MouseWeight", "GUI_OptoBrainRegion", "GUI_OptoStartState1",
+        ]
 
     if type(files_patterns) == str:
         files_patterns = [files_patterns]
