262cbcdcbcb2c1b8dad724c7ae89fee09c8787e1
Hatem Oraby <hatem.oraby@gmail.com>
2021-02-23 17:29:09 +0100

    Add analysis for evidence accumulation reaction-time

---
+++ report/evdaccum/evdutils.py
@@ -0,0 +1,151 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from report import analysis
+from report.clr import Choice, Difficulty as DifficultyClr
+
+def plotSides(df, *, col_name, friendly_col_name, animal_name,
+              quantile_top_bottom, y_label, save_figs,
+              save_prefix, save_postfix, legend_loc=None, plot_only_all=False):
+  df = df[df.ChoiceCorrect.notnull()]
+  REVERSED, NOT_REVERSED=True, False
+  # THe next part is a bad hack to group direction together, sorry about this...
+  df_incorrect = df[df.ChoiceCorrect == 0]
+  df_all = df.copy()
+  df_all.loc[df_incorrect.index, 'DV'] = -df_incorrect.DV
+  iter_list =    [(df_all, Choice.All, "All", NOT_REVERSED)]
+  if not plot_only_all:
+    iter_list += [
+          (df[df.ChoiceCorrect == 1], Choice.Correct,  "Correct", NOT_REVERSED),
+          (df_incorrect, Choice.Incorrect,"Incorrect", REVERSED)]
+  ROWS, COLS = (1, 2) if plot_only_all else (2, 2)
+  fig, axs = plt.subplots(ROWS, COLS)
+  fig.set_size_inches(COLS*analysis.SAVE_FIG_SIZE[0],
+                      ROWS*analysis.SAVE_FIG_SIZE[1])
+  top_row_axs = axs if plot_only_all else axs[0]
+  if not plot_only_all:
+    bottom_row_axs = axs[1]
+
+  for bar_idx, (side_df, color, label, li_is_reversed) in enumerate(iter_list):
+    kargs = Kargs(df=side_df, col_name=col_name, friendly_name=friendly_col_name,
+                  color=color, label=label,
+                  quantile_top_bottom=quantile_top_bottom,
+                  y_label=y_label, animal_name=animal_name)
+    for ax_idx, overlap_sides in enumerate([False, True]):
+      is_reversed = li_is_reversed and not overlap_sides
+      dv_count = metricVsDiff(axes=top_row_axs[ax_idx],
+                              overlap_sides=overlap_sides,
+                              is_reversed=is_reversed, **kargs)
+      if not plot_only_all:
+        BAR_WIDTH=0.06 * 100 # Convert to coherence
+        xs = np.array(list(dv_count.keys())) * 100
+        xs = np.around(xs)
+        # -1 depends on number of combinations to center bars
+        xs += BAR_WIDTH*(bar_idx-1)
+        # Normalze Ys
+        ys = np.array(list(dv_count.values()), dtype=np.float64)
+        ys /= ys.max()
+        label += " (Reversed)" if is_reversed else ""
+        bottom_row_axs[ax_idx].bar(xs, ys, color=color, width=BAR_WIDTH,
+                                   label=label)
+
+  if legend_loc:
+    top_row_axs[0].legend(loc=legend_loc,prop={'size':'x-small'})
+    top_row_axs[1].legend(loc=legend_loc,prop={'size':'x-small'})
+
+  if not plot_only_all:
+    for ax in bottom_row_axs:
+      ax.legend(prop={'size':'x-small'})
+      ax.set_title("Norm. Difficulties Dist. - {}".format(animal_name))
+      ax.set_xlabel("Coherence %")
+      ax.set_ylabel("Trials Count (Norm. per Difficulty")
+
+  if save_figs:
+    analysis.savePlot(save_prefix + animal_name + save_postfix)
+  plt.show()
+
+def grpByDifficulty(df, overlap_sides, is_reversed=False, emit_colors=False):
+  if is_reversed: assert overlap_sides == False
+  groupby_on = df.DV.abs() if overlap_sides else df.DV
+  dv_bins = np.linspace(0, 1, 4) if overlap_sides else np.linspace(-1, 1, 7)
+  colors =    [DifficultyClr.Hard, DifficultyClr.Med, DifficultyClr.Easy]
+  if overlap_sides:
+    colors += [DifficultyClr.Easy, DifficultyClr.Med, DifficultyClr.Hard]
+  grp_by_bins = pd.cut(groupby_on, dv_bins, include_lowest=not overlap_sides)
+  for color_idx, (dv_interval, dv_df) in enumerate(df.groupby(grp_by_bins)):
+    dv_val = dv_interval.left if dv_interval.left < 0 else dv_interval.right
+    if is_reversed: dv_val *= -1
+    yield (colors[color_idx], dv_val, dv_df) if emit_colors else (dv_val, dv_df)
+
+def fltrQuantile(df_or_col, quantile_top_bottom, col_name_if_df=None):
+  if isinstance(df_or_col, pd.DataFrame):
+    assert col_name_if_df is not None
+    col_vals = df_or_col[col_name_if_df]
+  else:
+    col_vals = df_or_col
+  return df_or_col[(col_vals.quantile(quantile_top_bottom) < col_vals) &
+                   (col_vals < col_vals.quantile(1 - quantile_top_bottom))]
+
+def shortLongWT(*, short_df, long_df, label, quantile, animal_name, axes):
+  #animal_name = " ".join(short_df.Name.unique()).strip()
+  axes_title = animal_name + " (Short-{} < {} quantile)".format(label, quantile)
+  PsycStim_axes = analysis.psychAxes(axes_title, axes=axes)
+  for data, color, title in [(short_df,'purple',"Short-{}".format(label)),
+                             (long_df,'blue',"Long-{}".format(label))]:
+    title += (f" - {len(data):,} pts ("
+              f"correct: {len(data[data.ChoiceCorrect==1]):,}, "
+              f"incorrect: {len(data[data.ChoiceCorrect==0]):,})")
+    LINE_SIZE=2
+    analysis._psych(data, PsycStim_axes, color, LINE_SIZE, title,
+                    plot_points=False, SEM=True)
+  PsycStim_axes.legend(prop={'size':'x-small'},loc='upper left')
+
+def plotShortLongWT(df, col_name, short_long_quantile, friendly_col_name,
+                    animal_name, save_figs, save_prefix, save_postfix):
+  ax = plt.axes()
+  short_df = df[df[col_name] <= df[col_name].quantile(short_long_quantile)]
+  long_df = df[df[col_name] > df[col_name].quantile(short_long_quantile)]
+  shortLongWT(short_df=short_df, long_df=long_df, label=friendly_col_name,
+              animal_name=animal_name, quantile=short_long_quantile, axes=ax)
+  if save_figs:
+    analysis.savePlot(save_prefix + animal_name + save_postfix)
+  plt.show()
+
+def Kargs(**kargs):
+  return kargs
+
+def metricVsDiff(df, *, col_name, friendly_name, axes, overlap_sides,
+                 quantile_top_bottom, animal_name, is_reversed=False,
+                 color=None, label="", y_label=None):
+  x_data = []
+  y_data = []
+  y_data_sem = []
+  count_pts = 0
+  dv_count = {}
+  for dv_val, dv_df in grpByDifficulty(df, overlap_sides, is_reversed=is_reversed):
+    cohr = round(dv_val* 100)
+    if quantile_top_bottom:
+      metric_fltrd = fltrQuantile(dv_df[col_name],
+                                  quantile_top_bottom=quantile_top_bottom)
+    else:
+      metric_fltrd = dv_df[col_name]
+    # print("Cohr:", cohr, "Cohr len:", len(dv_df), col_name, "mean:",
+    #       metric_fltrd.mean())
+    x_data.append(cohr)
+    y_data.append(metric_fltrd.mean())
+    y_data_sem.append(metric_fltrd.sem())
+    count_pts += len(metric_fltrd)
+    dv_count[dv_val] = len(metric_fltrd)
+
+  # print("X data:", x_data)
+  # print("y_data:", y_data)
+  # print("y_sem:", y_data_sem)
+  rvrsd_lbl = "(Reversed) " if is_reversed else ""
+  label = f"{friendly_name} {label} {rvrsd_lbl}({count_pts:,} pts)"
+  axes.errorbar(x_data, y_data, yerr=y_data_sem, color=color, label=label)
+  axes.set_title(f"{friendly_name} vs Difficulty - {animal_name}")
+  axes.set_xlabel("Coherence %")
+  if y_label is None: y_label = friendly_name
+  axes.set_ylabel(y_label)
+  axes.legend(loc="lower left", prop={'size': 'small'})
+  return dv_count
---
+++ report/evdaccum/reactiontime.py
@@ -0,0 +1,147 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from report.definitions import MouseState, ExpType
+from report import analysis
+from .evdutils import plotSides, plotShortLongWT
+
+def reactionTime(df, **kargs ):
+  cut_below_trial_num = kargs.pop("cut_below_trial_num")
+  for animal_name, animal_df in df.groupby(df.Name):
+    _processAnimal(animal_name, animal_df,
+                   cut_below_trial_num=cut_below_trial_num, **kargs)
+
+def _processAnimal(animal_name, animal_df, *, cut_below_trial_num, **kargs):
+  STIMULUS_TIME_MIN = 2
+  animal_df = animal_df[animal_df.GUI_StimAfterPokeOut == 0]
+  animal_df = animal_df[animal_df.GUI_MouseState != MouseState.FreelyMoving]
+  animal_df = animal_df[(animal_df.GUI_RewardAfterMinSampling == False) |
+                        (animal_df.CenterPortRewAmount == 0)]
+  # animal_df = animal_df[((animal_df.GUI_MinSampleType.isin([np.nan, 1])) &
+  #                        (animal_df.GUI_MinSampleMax < 0.5)) |
+  #                      ((animal_df.GUI_MinSampleType == 2) &
+  #                       (animal_df.GUI_MinSampleMin < 0.5))]
+  animal_df = animal_df[animal_df.MinSample < 0.75]
+  animal_df = animal_df[animal_df.ST.notnull()]
+  animal_df = animal_df[animal_df.GUI_StimulusTime >= STIMULUS_TIME_MIN]
+  animal_df = animal_df[animal_df.TrialNumber >= cut_below_trial_num]
+  if not len(animal_df) or animal_name in ["RDK_ThyF2", "N1"]:
+    return
+  print("Fonud: {} - Num trials: {:,}".format(animal_name, len(animal_df)))
+
+  st_df = animal_df
+  #st_df = st_df[(0.4 <= st_df.GUI_CalcLeftBias) & (st_df.GUI_CalcLeftBias <= 0.6)]
+  #st_df = st_df[st_df.GUI_MinSampleType != 4]
+  #st_df = st_df[st_df.SessionPerformance >= 70]
+  from utils import grpBySess
+  min_easiest_perf = kargs.pop("min_easiest_perf")
+  exp_type = kargs.pop("exp_type")
+  st_df = grpBySess(st_df).filter(_fltrSSn, exp_type=exp_type,
+                                  min_easiest_perf=min_easiest_perf)
+  if len(st_df) < 400:
+    return
+
+  save_prefix = kargs.pop("save_prefix")
+  _reactionTimePerDF(animal_name=animal_name, df=st_df, save_prefix=save_prefix,
+                     **kargs)
+  for sess_info, sess_df in grpBySess(st_df):
+    print(animal_name, "Date", sess_info)
+    name = f"{animal_name}_{sess_info[0]}_Sess{sess_info[1]}"
+    print("Name:", name)
+    new_save_prefix = save_prefix + f"/{animal_name}_sess/"
+    _reactionTimePerDF(animal_name=name, df=sess_df,
+                       save_prefix=new_save_prefix, **kargs)
+
+def _fltrSSn(sess_df, exp_type, min_easiest_perf):
+  sess_df = sess_df[sess_df.GUI_ExperimentType == exp_type]
+  df_choice = sess_df[sess_df.ChoiceCorrect.notnull()]
+  df_choice = df_choice[df_choice.Difficulty3.notnull()]
+  if len(df_choice) < 50:
+    print(f"Insufficient trials ({len(df_choice)}) for "
+          f"{sess_df.Date.iloc[0]}-Sess{sess_df.SessionNum.iloc[0]}")
+    return False
+  trial_difficulty_col = df_choice.DV.abs() * 100
+  if exp_type == ExpType.RDK:
+    trial_difficulty_col = (trial_difficulty_col-50)*2
+  easiest_diff = df_choice[trial_difficulty_col == df_choice.Difficulty1]
+  if len(easiest_diff):
+    easiest_perf = \
+      len(easiest_diff[easiest_diff.ChoiceCorrect == 1]) / len(easiest_diff)
+  else:
+    easiest_perf = -1
+  easiest_perf *= 100
+  if easiest_perf < min_easiest_perf:
+    print(f"Bad performance ({easiest_perf:.2f}%) for "
+          f"{sess_df.Date.iloc[0]}-Sess{sess_df.SessionNum.iloc[0]} - "
+          f"Len: {len(df_choice)}")
+  return easiest_perf >= min_easiest_perf
+
+def _reactionTimePerDF(animal_name, df, *, quantile_top_bottom,
+                       short_long_quantile, save_figs, save_prefix):
+  df_valid_st = df[df.ST.notnull()]
+  del df # We don't need it from this point it, raise an error if it was used
+  # Subtract 5ms, as sometimes Bpod might give a little bit earlier time than
+  # expected
+  df_overstay = df_valid_st[df_valid_st.ST >=
+                            df_valid_st.GUI_StimulusTime - 0.005]
+  df_accepted = df_valid_st[~df_valid_st.index.isin(df_overstay.index)]
+  print("Reaction time len:", len(df_accepted),
+        "- MinSamplingMax dist:", df_valid_st.GUI_MinSampleMax.value_counts())
+
+  fig, (ax_min_sampling, ax_st_time) = plt.subplots(1,2)
+  fig.set_size_inches(2*analysis.SAVE_FIG_SIZE[0], 1*analysis.SAVE_FIG_SIZE[1])
+  # Break each second to 20 bins
+  ax_min_sampling.hist(df_valid_st.MinSample,
+                       bins=int(np.ceil(df_valid_st.MinSample.max() * 20)))
+  ax_min_sampling.set_title("Min. Sampling Dist - {}".format(animal_name))
+  ax_min_sampling.set_xlim(xmin=0, xmax=2)
+  # Break each second to 4 bins
+  overstay = df_overstay.ST + df_overstay.calcReactionTime
+  overstay[overstay > 10] = 10
+  max_st = overstay.max() if len(overstay) else df_accepted.ST.max()
+  num_hist_bins = int(np.ceil(max_st*4))
+  print("Hist bins:", num_hist_bins)
+  accepted_correct = df_accepted[df_accepted.ChoiceCorrect == True].ST
+  accepted_incorrect = df_accepted[df_accepted.ChoiceCorrect == False].ST
+  ax_st_time.hist([accepted_correct, accepted_incorrect, overstay], bins=num_hist_bins,
+                   stacked=True, color=['lime', 'r', 'k'],
+                   label=["Correct", "Incorrect", "Overstay"])
+  FILTER_QUANTILE = False
+  if FILTER_QUANTILE:
+    for quantile in [0.99]:#[0.75, 0.9, 0.95, 0.99]:
+      quantile_val = df_valid_st.ST.quantile(quantile)
+      x = np.around(quantile_val*4)/4
+      print(f"quantile {quantile} = {quantile_val} - X: {x}")
+      ax_st_time.axvline(x, linestyle='dashed', label=f'{quantile} Quantile', color='k')
+  ax_st_time.legend(loc='upper right')
+  ax_st_time.set_title("Sampling Time Dist - {}".format(animal_name))
+  ax_st_time.set_xlim(xmin=0, xmax=10)
+  if save_figs:
+    analysis.savePlot(save_prefix + animal_name + "_sampling_hist")
+  plt.show()
+
+  if FILTER_QUANTILE:
+    df_accepted = df_valid_st[df_valid_st.ST <=
+                              df_valid_st.ST.quantile(quantile)]
+  PsycStim_axes = analysis.psychAxes(animal_name)
+  analysis.psychAnimalSessions(df_accepted, animal_name, PsycStim_axes,
+                               analysis.METHOD)
+  if save_figs:
+    analysis.savePlot(save_prefix + animal_name + "_sampling_data_psych")
+  plt.show()
+
+  plotSides(df_accepted, col_name="ST", friendly_col_name="Sampling Time",
+             animal_name=animal_name, y_label="Sampling Time (S)",
+             quantile_top_bottom=quantile_top_bottom,
+             save_figs=save_figs, save_prefix=save_prefix,
+             save_postfix="_sampling_vs_diff")
+
+  plotShortLongWT(df_accepted, col_name="ST", friendly_col_name="Sampling Time",
+                  animal_name=animal_name, save_postfix="_sampling_short_long",
+                  short_long_quantile=short_long_quantile,
+                  save_figs=save_figs, save_prefix=save_prefix)
+
+  plotSides(df_accepted, col_name="calcMovementTime", friendly_col_name="Movement Time",
+             animal_name=animal_name, y_label="Movement Time (S)",
+             quantile_top_bottom=quantile_top_bottom, legend_loc="upper center",
+             save_figs=save_figs, save_prefix=save_prefix,
+             save_postfix="_sampling_movement_vs_diff")
